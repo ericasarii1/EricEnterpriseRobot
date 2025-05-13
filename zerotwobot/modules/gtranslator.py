535ce1f0d477dc05119fe884567e68011c4aec7e
ericasarii1 <nang.tamvan@gmail.com>
2025-05-13 09:26:17 +0700

    Update gtranslator.py

--- zerotwobot/modules/gtranslator.py
+++ zerotwobot/modules/gtranslator.py
@@ -90,7 +90,7 @@ async def totranslate(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 __help__ = """
-• `/tr` or `/tl` (language code) as reply to a long message
+❂ `/tr` or `/tl` (language code) as reply to a long message
 *Example:*
   `/tr en`*:* translates something to english
   `/tr hi-en`*:* translates hindi to english
